6f6c47dae3d858dd23b0cea943f2bd3d8413d9da
rifqirowi33 <rifqirowi3333@gmail.com>
2025-07-01 16:20:30 +0700

    maintenance path route

--- backend/games.js
+++ backend/games.js
@@ -2,7 +2,7 @@ export const games = [
   {
     id: "monster-hunter",
     name: "Monster Hunter: The Blood of Red Dragon Indonesia",
-    key: "Monster_Hunter_The_Blood_Of_Red_Dragon_ID_240x320.jar",
+    key: "games/Monster_Hunter_The_Blood_Of_Red_Dragon_ID_240x320.jar",
     size: 1320000,
     year: 2025,
     screen: "240×320",
